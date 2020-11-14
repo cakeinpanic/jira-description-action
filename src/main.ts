1bb947e043c87392aa6dcb6e1ae0af18542d62a8
cakeinpanic <cakeinpanic@gmail.com>
2020-11-14 19:36:30 +0200

    debug

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,28 @@
 import * as core from '@actions/core';
-import { shouldSkipBranch } from './utils';
-import { getInputs } from './action-inputs';
 import { GithubConnector } from './github-connector';
+
 //import { JiraConnector } from './jira-connector';
 
 async function run(): Promise<void> {
   try {
-    const { BRANCH_IGNORE_PATTERN } = getInputs();
+    //const { BRANCH_IGNORE_PATTERN } = getInputs();
 
     const githubConnector = new GithubConnector();
     //const jiraConnector = new JiraConnector();
-
-    if (!githubConnector.isPRAction) {
-      console.log('This action meant to be run only on PRs');
-      process.exit(0);
-    }
-
-    if (shouldSkipBranch(githubConnector.headBranch, BRANCH_IGNORE_PATTERN)) {
-      process.exit(0);
-    }
+    //
+    //if (!githubConnector.isPRAction) {
+    //  console.log('This action meant to be run only on PRs');
+    //  process.exit(0);
+    //}
+    //
+    //if (shouldSkipBranch(githubConnector.headBranch, BRANCH_IGNORE_PATTERN)) {
+    //  process.exit(0);
+    //}
 
     const issueKey = githubConnector.getIssueKeyFromTitle();
 
     if (!issueKey) {
+      console.log(`JIRA key was not found`);
       process.exit(0);
     }
 
