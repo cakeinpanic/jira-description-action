1da4052fab21d9692e4a9b8be8c082ce90fe6a4a
Gregory Schofield <greg.c.schofield@gmail.com>
2023-11-02 10:19:29 -0400

    Output a JIRA key if found.
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -16,12 +16,12 @@ async function run(): Promise<void> {
 
     if (!githubConnector.isPRAction) {
       console.log('This action meant to be run only on PRs');
-      setOutputs(false, null);
+      setOutputs(null, null);
       process.exit(0);
     }
 
     if (shouldSkipBranch(githubConnector.headBranch, BRANCH_IGNORE_PATTERN)) {
-      setOutputs(false, null);
+      setOutputs(null, null);
       process.exit(0);
     }
 
@@ -30,11 +30,11 @@ async function run(): Promise<void> {
     const details = await jiraConnector.getTicketDetails(key);
     await githubConnector.updatePrDetails(details);
 
-    setOutputs(true, source);
+    setOutputs(key, source);
   } catch (error) {
     console.log('Failed to add JIRA description to PR.');
     core.error(error.message);
-    setOutputs(false, null);
+    setOutputs(null, null);
     if (FAIL_WHEN_JIRA_ISSUE_NOT_FOUND) {
       core.setFailed(error.message);
       process.exit(1);
@@ -44,7 +44,9 @@ async function run(): Promise<void> {
   }
 }
 
-function setOutputs(isFound: boolean, source: ESource | null): void {
+function setOutputs(key: string | null, source: ESource | null): void {
+  var isFound = key !== null;
+  core.setOutput('jira-issue-key', key);
   core.setOutput('jira-issue-found', isFound);
   core.setOutput('jira-issue-source', source || 'null');
 }
